e9333e4df496d3348ceebfc8f3fadc10b838eee2
elyasaf <elyasafklein972>
2020-12-10 14:26:55 +0200

    App2

---
+++ app/src/main/java/il/co/expertize/emailauthfirebase/MainApp/MainActivity.java
@@ -0,0 +1,50 @@
+/**
+ * Code based on:
+ * URL 1: https://blog.usejournal.com/firebase-email-and-password-authentication-for-android-e335c81a1dad
+ * URL 2: https://github.com/buildbro/FirebaseEmailPasswordExample
+ */
+
+package il.co.expertize.emailauthfirebase.MainApp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import il.co.expertize.emailauthfirebase.R;
+import il.co.expertize.emailauthfirebase.UI.LoginActivity;
+import il.co.expertize.emailauthfirebase.UI.CreateAccount;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button registerBtn, loginBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        initializeViews();
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CreateAccount.class);
+                startActivity(intent);
+            }
+        });
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initializeViews() {
+        registerBtn = findViewById(R.id.register);
+        loginBtn = findViewById(R.id.login);
+    }
+}
---
+++ app/src/main/java/il/co/expertize/emailauthfirebase/UI/NavigationDrawerActivity.java
@@ -0,0 +1,16 @@
+package il.co.expertize.emailauthfirebase.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import il.co.expertize.emailauthfirebase.R;
+
+public class NavigationDrawerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nevigation_drawer);
+    }
+}
